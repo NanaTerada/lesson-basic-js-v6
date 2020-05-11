08e7c52e2f4fbb8626dcede3aa86b84a93e8e5f7
n2 <7worki.nprogress@gmail.com>
2020-05-11 12:31:08 +0900

    もっと短く書く！条件演算子

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 // trueだったときの処理
 // }
 
-const score = 85;
+const score = 80;
+
 if(score > 80){
   console.log('Great!');
 } else if (score >= 60){
@@ -22,4 +23,9 @@ if(score > 80){
 //         それ以外の場合の処理
 // }
 
+// もっと短く書く！条件演算子
+// 構文↓
+// 条件式　？　true　の処理　：falseの処理
+
+score >= 80 ? console.log('Great!') : console.log('OK...');
         
