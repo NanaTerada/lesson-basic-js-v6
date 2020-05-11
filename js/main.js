3cb3e2fb81209bb91bf72405234330eb7890130f
n2 <7worki.nprogress@gmail.com>
2020-05-11 15:06:41 +0900

    returnで値を返してみよう

--- js/main.js
+++ js/main.js
@@ -1,40 +1,15 @@
 'use strict';
 
-// 間に広告を入れたいとき
-
-console.log('----------');
-console.log('-----AD---');
-console.log('----------');
-console.log('Tom is great!');
-console.log('BOb is great!');
-console.log('----------');
-console.log('-----AD---');
-console.log('----------');
-console.log('Steve is great!');
-console.log('Richard is great!');
-console.log('----------');
-console.log('-----AD---');
-console.log('----------');
-
-
-// 関数を使うと短く書ける
-
-//引数を使ってみよう
-
-function showAd(message = 'AD'){　//仮引数 実引数が無かった時の値を設定できる
-        console.log('----------');
-        console.log(`-----${message}---`);
-        console.log('----------');   
+function sum(a,b,c) { //カンマ区切りでいくつでも渡せる
+        //console.log(a + b + c);
+        return a + b + c;  returnを書くとそれ以降の処理は実行されないので注意
 }
 
-showAd('header Ad');　//実引数
-console.log('Tom is great!');
-console.log('BOb is great!');
-showAd();
-console.log('Steve is great!');
-console.log('Richard is great!');
-showAd('fooer Ad');
+sum(1,2,3);
+sum(3,4,5);
 
+const total = sum(1,2,3) + sum(3,4,5);
+console.log(total);
 
 
         
