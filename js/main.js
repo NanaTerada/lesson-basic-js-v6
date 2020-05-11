51aef3a8c74648d46c7469e204f26391b1d9db05
n2 <7worki.nprogress@gmail.com>
2020-05-11 12:58:05 +0900

    forを使ったループ処理をしよう

--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
 'use strict';
 
+// iはカウンターの変数名によく使われる。再代入されるのでlet
 
-const signal = 'green';
-
-if( signal === "red"){
-        console.log('stop!');
-} else if( signal === "yello"){
-        console.log('Caution!');
-} else if( signal === "blue"){
-        console.log('Go!');
-}
-
-switch(signal){
-        case "red" :
-          console.log('stop!');
-          break
-        case "yello" :
-          console.log('Caution!');
-          break;
-        case "blue" :
-        case "green": //もしくは
-          console.log('Go!');
-          break;
-          default:
-        console.log('wrong signal');
-        break;
+for (let i = 1; i <= 10; i++){
+// console.log("hello");
+// console.log("hello" + i);
+// テンプレートリテラルを使う↓``と$を使う
+console.log(`hello ${i}`);
 
 }
+
+
+
 
 
         
