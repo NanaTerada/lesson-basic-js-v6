b81b954d7b5b12995ceb0c380897d9b032a33b63
n2 <7worki.nprogress@gmail.com>
2020-05-11 11:50:48 +0900

    変数を使ってみよう

--- js/main.js
+++ js/main.js
@@ -28,11 +28,30 @@
         // なんの計算をしているのかわかりづらいので名前を付けよう
 
         // 値を割り当てるを代入という
-        const price = 150;　//この定数を使いますよ～宣言をする
+        //const price = 150;　//この定数を使いますよ～宣言をする
         // 右の値を　左の定数に割り当てる。イコールではない
         // これ以降priceは150になる
         // あとから修正するときにいっきに変更できルので楽
 
-        console.log(price * 140);
-        console.log(price * 160);
+        // console.log(price * 140);
+        // console.log(price * 160);
 
+        // constでは再代入ができない
+        // 変数を使ってみよう
+
+        // varという書き方もあるが古いのでletを使っていく
+        // 英数字　＄　＿のみ
+        // 数値から始めれない
+        // 予約後（let const）は使えない
+
+        // let price = 170; 
+        // console.log(price * 140);
+        // console.log(price * 160);
+
+        let price = 500;
+        price = price + 100;
+        // ↓短くかくには
+        price += 100;
+        // 1足したい場合は特殊な書き方がある
+        price++;
+        price--;
