b223cc72ce23844dd4ba09e1f372471c047ca061
n2 <7worki.nprogress@gmail.com>
2020-05-11 12:38:59 +0900

    論理演算子を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
 'use strict';
 
-// if (条件文){
-// trueだったときの処理
-// }
+const score = 60;
+const name ='taguchi';
 
-const score = 80;
+if(score >= 50){
+        if(name === 'taguchi'){
 
-if(score > 80){
-  console.log('Great!');
-} else if (score >= 60){
- console.log("Good");
-} else{
-     console.log("OK...");   
+                console.log("Good job!");
+        }
 }
-// 命令がブロックで終わる場合は；不要
 
-// if (条件文A){
-// 条件文Atrueだったときの処理
-// } else if(条件文B){
-//  条件文Aはfalse条件文Btrueだったときの処理
-// } else {
-//         それ以外の場合の処理
-// }
+// && なおかつ (AND)
+// || もしくは　(OR)
+// !　～ではない　(NOT)
 
-// もっと短く書く！条件演算子
-// 構文↓
-// 条件式　？　true　の処理　：falseの処理
 
-score >= 80 ? console.log('Great!') : console.log('OK...');
+if (score >= 50 && name === 'taguchi'){
+        console.log("Good job!");
+} 
+
+
+
         
