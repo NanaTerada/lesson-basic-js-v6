c0693aa727dd097a9f11943fdde801f94436c04b
n2 <7worki.nprogress@gmail.com>
2020-05-11 15:25:16 +0900

    関数式を使ってみよう

--- js/main.js
+++ js/main.js
@@ -12,4 +12,14 @@ const total = sum(1,2,3) + sum(3,4,5);
 console.log(total);
 
 
+// 関数式でかいてみする
+
+const sum2 = function(e,f,g){
+        return e + f + g;
+}; //セミコロン注意
+ 
+const total2 = sum2(1,2,3) + sum2(3,4,5);
+console.log(total2);
+
+
         
