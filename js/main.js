ed2b9aa2d576c42086802bf6faa2269afbfd2cac
n2 <7worki.nprogress@gmail.com>
2020-05-11 11:21:46 +0900

    文字列を扱う

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+        console.log('Hello World from main.js!');
+        
+        console.log("it's me!");
+        // シングルクォーテーションが間に入る場合は””で囲う
+        console.log('it\'s me!');
+        // \(バックスラッシュ)の直後のシングルクォーテーションは文字として扱われる
+        console.log('He\nllo Wor\tld');
+        // \ｎ　は　開業　\ｔ　はテーブル
+        console.log('hello' + 'world');
+        // 文字の連結は　+　を使う
